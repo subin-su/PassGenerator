edabda5d4ddfd71a6e01589621ba73f596896eee
subinkc <subinkc140@gmail.com>
2020-12-11 14:43:22 -0800

    prompts

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
 let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
+if(length<8||length>128){
+    alert("Please enter pass between 8 and 128");
+    
+}
+else{
+    let includesymbols= confirm("Do you wants to have symbols in your password?");
+    let includeupperCase= confirm("Do you wants to have uppercase in your password?");
+    let includelowerCase= confirm("Do you wants to have lowercase in your password?");
+    let includenumbers= confirm("Do you wants to have numbers in your password?");
+    if (!includesymbols && !includenumbers && !includelowerCase && !includeupperCase)
+    {
+      alert("please try again with atlease one uppercase,one number,one symbols and onelowercase");
+      return;
+    
+    
+    }
+}
 
-//event makes user to click to generate button
-generatepasswordBtn.addEventListener("click", );
+    
