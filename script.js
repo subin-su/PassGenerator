d6ee2c4cc2273cec1f88c457f86df3cde63ceecc
subinkc <subinkc140@gmail.com>
2020-12-11 15:15:37 -0800

    Options

--- script.js
+++ script.js
@@ -4,27 +4,25 @@
 let generatepasswordBtn = document.querySelector("#generate");
 
 var length=prompt("How long do you want your password ?");
+function enable(){
+    let symbols= confirm("Do you wants to have symbols in your password?");
+    let upperCase= confirm("Do you wants to have uppercase in your password?");
+    let lowerCase= confirm("Do you wants to have lowercase in your password?");
+    let numbers= confirm("Do you wants to have numbers in your password?");
+    
+}
 
 let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
 let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
 if(length<8||length>128){
-    alert("Please enter pass between 8 and 128");
+    alert("Please enter pass between 8 and 128");}
+  
     
-}
 else{
-    let includesymbols= confirm("Do you wants to have symbols in your password?");
-    let includeupperCase= confirm("Do you wants to have uppercase in your password?");
-    let includelowerCase= confirm("Do you wants to have lowercase in your password?");
-    let includenumbers= confirm("Do you wants to have numbers in your password?");
-    if (!includesymbols && !includenumbers && !includelowerCase && !includeupperCase)
-    {
-      alert("please try again with atlease one uppercase,one number,one symbols and onelowercase");
-      return;
-    
-    
-    }
+    enable();
 }
+
 
     
