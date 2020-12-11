3e63d0f2b04a4fc65f1ab9b64c6c4cf26969bfe3
subinkc <subinkc140@gmail.com>
2020-12-11 14:09:58 -0800

    Added the letters and symbols

--- script.js
+++ script.js
@@ -5,3 +5,7 @@
 
 var length=prompt("How long do you want your password ?");
 
+let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
+let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
