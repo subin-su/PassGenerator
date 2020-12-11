24b70538c1539df51423d1c94ac2392cc6421fd1
subinkc <subinkc140@gmail.com>
2020-12-11 14:21:40 -0800

    event

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 // program started
 
-
+let generatepasswordBtn = document.querySelector("#generate");
 
 var length=prompt("How long do you want your password ?");
 
@@ -9,3 +9,6 @@ let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O",
 let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
 let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
+
+//event makes user to click to generate button
+generatepasswordBtn.addEventListener("click", );
