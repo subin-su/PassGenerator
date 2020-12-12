0023febfa6eb414b0292b2202bb00d3b1b99ccca
subinkc <subinkc140@gmail.com>
2020-12-11 23:05:10 -0800

    trying to push enetered length pass

--- script.js
+++ script.js
@@ -3,30 +3,45 @@
 
 let generatepasswordBtn = document.querySelector("#generate");
 
+
+function first(){
+
 var length=prompt("How long do you want your password ?");
-
-
 let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
 let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
-if(length<8||length>128){
-    alert("Please enter pass between 8 and 128");}
+
+    
+    if(length<8||length>128){
+        alert("Please enter pass between 8 and 128");
+        return;
+    
+    
+    }
   else{
-    alert("Choose confirm to procced ahead and cancel to terminate");
+    return;
+    
   }
-    let symbols= confirm("Do you wants to have symbols in your password?");
-    let upperCase= confirm("Do you wants to have uppercase in your password?");
-    let lowerCase= confirm("Do you wants to have lowercase in your password?");
-    let numbers= confirm("Do you wants to have numbers in your password?");
+let symbols= confirm("Do you wants to have symbols in your password?");
+let upperCase= confirm("Do you wants to have uppercase in your password?");
+let lowerCase= confirm("Do you wants to have lowercase in your password?");
+let numbers= confirm("Do you wants to have numbers in your password?");
+    
     if(!symbols&&!upperCase&&!lowerCase&&!numbers){
-        alert("You should confirm atleast one")
+        alert("You should confirm atleast one");
+        return;
     }
     else{
         alert("Wait till the password is generating");
-    }
+    }}
+
+
  
-    
+     let x=first();
+     if(!x){
+         return;
+     }
  let  password=[];
         var choice1=onlyupperCase[Math.floor(Math.random()*onlyupperCase.length)];
     var choice2=onlylowerCase[Math.floor(Math.random()*onlylowerCase.length)];
@@ -34,6 +49,8 @@ if(length<8||length>128){
     var choice4=onlysymbols[Math.floor(Math.random()*onlysymbols.length)];
      if(!symbols&&!upperCase&&!lowerCase&&!numbers){
        alert("Choose atleast 1 OK to continue");}
+    
+       //
        if(symbols){
            password.push(choice4);
        }
@@ -46,9 +63,14 @@ if(length<8||length>128){
     if(numbers){
         password.push(choice3);
     }
+    
  
 
  alert(password);
+ for(var j=0;j<length;i++){
+ var choice20=password[Math.floor(Math.random()*password.length)];
+ password.push(choice20);
+ alert(password);}}
   
     
 
