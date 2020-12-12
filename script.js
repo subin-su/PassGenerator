defa8b8801aec0c39de29045836a6150d711168b
subinkc <subinkc140@gmail.com>
2020-12-12 11:16:07 -0800

    Finally able to generate pass, thanks to w3 schools

--- script.js
+++ script.js
@@ -1,63 +1,92 @@
-// Assignment Code
-// program started
 
 let generatepasswordBtn = document.querySelector("#generate");
-alert("Click below button to start");
-
-function first(){
-   
-
-
-var length=prompt("How long do you want your password ?");
+// These are the possible elemets extracted from a website
 let onlyupperCase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 let onlylowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 let onlynumbers=["0","1","2","3","4","5","6","7","8","9","0"]
 let onlysymbols=["!","@","#","$","%","&","^","*","+","?"]
+// The instruction page//
 
-    
-    if(length<8||length>128){
-        alert("Please enter pass between 8 and 128");
-        return;
-    
-    
-    }
-  else{
-    alert("Choose more options")
-    
+ alert("Welcome! Please click 'Generate password' to start!");
+ //function here accumulates all the choices user wants and later when called with another function produces pass//
+function first()
+{
+    // prompt the user to enter password size
+
+ var length=prompt("Hoe liong do you want your password ?");
+  if(length>128 || length<8){
+      alert("Choose a number bewtwwn 8 and 125");
+      return;
   }
-let symbols= confirm("Do you wants to have symbols in your password?");
-let upperCase= confirm("Do you wants to have uppercase in your password?");
-let lowerCase= confirm("Do you wants to have lowercase in your password?");
-let numbers= confirm("Do you wants to have numbers in your password?");
-    
-    if(!symbols&&!upperCase&&!lowerCase&&!numbers){
-        alert("You should confirm atleast one");
-        return;
-    }
-    else{
-        alert("Wait till the password is generating");
-    }
-
+  else{
+      alert("Lets proceed on!!!!!!!!!!!")
+  }
+  //These arr the options provided to the users
+  let includesymbols= confirm("Do you wants to have symbols in your password?");
+  let includeupperCase= confirm("Do you wants to have uppercase in your password?");
+  let includelowerCase= confirm("Do you wants to have lowercase in your password?");
+  let includenumbers= confirm("Do you wants to have numbers in your password?");
+  if (!includesymbols && !includenumbers && !includelowerCase && !includeupperCase)
+  {
+    alert("Choose atleast one of the options");
+    return;
+  
+  
+  }
+  else{
+      alert("Generating password for")
+  }
+ 
+let allvalue={
+  length: length,
+  lowerCase:includelowerCase,
+  upperCase: includeupperCase,
+  numeric:includenumbers,
+  specialcharacters: includesymbols
+}
+return allvalue;
 }
 
-function password(){
-    let x=first();
-    if (!x) {
-       return;
+// Write password to the #password input
+function two(){
+ let x=first();
+ if (!x) {
+    return;
+ }
+//  help to extract the password
+  let password1=[];
+  console.log(password1)
+  if (x.lowerCase){
+    for (let i = 0; i < onlylowerCase.length; i++) {
+      password1.push(onlylowerCase[i]);
     }
-    var choice1=onlyupperCase[Math.floor(Math.random()*onlyupperCase.length)];
-    var choice2=onlylowerCase[Math.floor(Math.random()*onlylowerCase.length)];
-    var choice3=onlynumbers[Math.floor(Math.random()*onlynumbers.length)];
-    var choice4=onlysymbols[Math.floor(Math.random()*onlysymbols.length)];
-    let password1=[];
-    if(x.symbols){
-        password1.push(choice4);}
-        if(x.numbers){
-            password1.push(choice3);}
-            if(x.upperCase){
-                password1.push(choice1);}
-                if(x.lowerCase){
-                    password1.push(choice42);}
-
-        
-    
+  }
+  if (x.upperCase){
+    for (let i = 0; i <onlyupperCase.length; i++) {
+       password1.push(onlyupperCase[i]);
+    }
+  }
+  if (x.numeric){
+    for (let i = 0; i <onlynumbers.length; i++) {
+        password1.push(onlynumbers[i]);
+    }
+  }
+  if (x.specialcharacters){
+    for (let i = 0; i < onlysymbols.length; i++) {
+        password1.push(onlysymbols[i]);
+  }
+}
+ let lastpassword=[];
+ for (let i = 0; i < x.length; i++) {
+   let pickrandomly =Math.floor(Math.random()*Math.floor(password1.length));
+   lastpassword.push(password1[pickrandomly]);
+ }
+ console.log(lastpassword)
+ let finalpassword=lastpassword.join("");
+ console.log(finalpassword);
+ document.getElementById("password").value = finalpassword;
+  } 
+  
+ 
+// Add event listener to generate button
+generatepasswordBtn.addEventListener("click", two);
